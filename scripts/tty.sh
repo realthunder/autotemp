852ba90e572cfe82dbf4168c7107d24e59fad517
Zheng, Lei <realthunder.dev@gmail.com>
2014-05-07 10:04:06 +0800

    Removed faulty lock in monitor script

--- scripts/tty.sh
+++ scripts/tty.sh
@@ -1,10 +1,8 @@
 TTYDEV=/dev/ttyACM0
 TTYLOCK=/tmp/`basename $TTYDEV`.lock
-TTYREADLOCK=/tmp/`basename $TTYDEV`.rlock
 
 if [ -z "$TTYINCLUDED" ]; then
   exec 100> $TTYLOCK
-  exec 101> $TTYREADLOCK
   TTYINCLUDED=1
 fi
 
@@ -17,34 +15,25 @@ tty_unlock() {
 }
 
 tty_read()  {
-
-  # If we need to read the tty output after sending
-  # a command, we use this read lock to ensure the command
-  # sending starts after we've ready to receive the data.
-  flock 101
   (
     exec < $TTYDEV
     stty igncr
     
-    # unlock once we are ready
-    flock -u 101
-    
     read line
     while read line; do
-      [ "$line" = ">" ] && break
-      echo $line
+    [ "$line" = ">" ] && break
+    echo $line
     done
-  ) &
+  )
 }
 
 tty_send() {
-  # The lock is harmless, if we don't need to read any feedback
-  flock 101
   echo "$@" > $TTYDEV
 }
 
 tty_run() {
-  tty_read
+  tty_read &
+  sleep 0.5 &> /dev/null || sleep 1
   tty_send "$@"
   wait
 }
